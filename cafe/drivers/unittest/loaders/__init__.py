7ad1e67c362f9c49724983e849d69938abe5bc5c
Daryl Walleck <daryl.walleck@rackspace.com>
2013-04-17 01:48:28 -0700

    Added missing license headers
    Change-Id: Ie70e8e778a2e0f8063fe14f179635a03e5678d95

--- cafe/drivers/unittest/loaders/__init__.py
+++ cafe/drivers/unittest/loaders/__init__.py
@@ -1,5 +1,15 @@
-'''
-@summary: Classes and Utilities for adapters that provide low level connectivity to various resources
-@note: Most often consumed by a L{cafe.engine.clients} or L{cafe.common.reporting}
-@note: Should not be used directly by a test case or process 
-'''
+"""
+Copyright 2013 Rackspace
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
