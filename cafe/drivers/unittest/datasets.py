eb42ed58a7ad49061ef140a848a3913df86ebc3b
Jose Idar <jose.idar@rackspace.com>
2013-05-24 12:22:50 -0500

    Add support for data-driven unittests.
    Makes it possible to parameterize a unittest test case method
    and provide several sets of values for those test parameters so
    as to execute that test for every set of data provided.
    
    A test method needs to be named with the "ddtest_" prefix instead
    of the unittest standard "test_" prefix to distinguish it from
    regular unittest test case methods.
    
    A test method needs to be decorated with the "data_driven_test"
    decorator, and a dataset source provided to the decorator.
    
    The unittest.TestCase class containing data driven tests must be
    decorated with the "DataDrivenFixture" decorator.
    
    Adds a unittest.TestCase class decorator, "DataDrivenFixture", to
    apply the test multiplication logic.
    
    Adds a test method decorator, "data_driven_test", to define the
    datasource a data driven test will use, and denote that test as
    a data driven test.
    
    Adds a Dataset object, a class that defines a dictionary
    to hold the keyword argument values for a data driven test, and
    the name of the dataset itself.
    
    Adds a module, "datasets.py" that defines several classes for
    providing various user-defined and generic datasets to data driven
    tests via list-like iterators.
    
    Change-Id: I7f03500b4c72f11d2cfe7dea7be2ffed8a5b8981

---
+++ cafe/drivers/unittest/datasets.py
@@ -0,0 +1,92 @@
+"""
+Copyright 2013 Rackspace
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+import json
+
+
+class Dataset(object):
+    def __init__(self, name, data_dict):
+        """Defines a set of data to be used as input for a data driven test.
+
+        data_dict should be a dictionary with keys matching the keyword
+        arguments defined in test method that consumes the dataset.
+
+        name should be a string describing the dataset.
+        """
+
+        self.name = name
+        self.data = data_dict
+
+
+class DatasetList(list):
+    """Specialized list-like object that holds Dataset objects"""
+
+    def append(self, dataset):
+        if not isinstance(dataset, Dataset):
+            raise TypeError(
+                "append() argument must be type Dataset, not {0}".format(
+                    type(dataset)))
+
+        super(DatasetList, self).append(dataset)
+
+    def append_new_dataset(self, name,  data_dict):
+        """Creates and appends a new Dataset"""
+        self.append(Dataset(name, data_dict))
+
+
+class DatasetGenerator(DatasetList):
+    """Generates Datasets from a list of dictionaries, which are named
+    numericaly according to the source dictionary's order in the source list.
+    If a base_dataset_name is provided, that is used as the base name postfix
+    for all tests before they are numbered.
+    """
+
+    def __init__(self, list_of_dicts, base_dataset_name=None):
+        count = 0
+        for kwdict in list_of_dicts:
+            test_name = "{0}_{1}".format(base_dataset_name or "dataset", count)
+            self.append_new_dataset(test_name, kwdict)
+            count += 1
+
+
+class TestMultiplier(DatasetList):
+    """Creates num_range number of copies of the source test,
+    and names the new tests numerically.  Does not generate Datasets.
+    """
+
+    def __init__(self, num_range):
+        for num in range(num_range):
+            name = "{0}".format(num)
+            self.append_new_dataset(name, dict())
+
+
+class DatasetFileLoader(DatasetList):
+    """Reads a file object's contents in as json and converts them to
+    lists of Dataset objects.
+    Files should be opened in 'rb' (read binady) mode.
+    File should be a list of dictionaries following this format:
+    [{'name':"dataset_name", 'data':{key:value, key:value, ...}},]
+    if name is ommited, it is replaced with that dataset's location in the
+    load order, so that not all datasets need to be named.
+    """
+    def __init__(self, file_object):
+        content = json.loads(str(file_object.read()))
+        count = 0
+        for dataset in content:
+            name = dataset.get('name', str(count))
+            data = dataset.get('data', dict())
+            self.append_new_dataset(name, data)
+            count += 1
--- cafe/drivers/unittest/decorators.py
+++ cafe/drivers/unittest/decorators.py
@@ -13,15 +13,90 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 """
+
+from types import FunctionType
+from unittest2 import TestCase
+
+
 TAGS_DECORATOR_TAG_LIST_NAME = "__test_tags__"
 TAGS_DECORATOR_ATTR_DICT_NAME = "__test_attrs__"
+DATA_DRIVEN_TEST_ATTR = "__data_driven_test_data__"
+DATA_DRIVEN_TEST_PREFIX = "ddtest_"
+
+
+class DataDrivenFixtureError(Exception):
+    pass
 
 
 def tags(*tags, **attrs):
-    def wrap(func):
+    """Adds tags and attributes to tests, which are interpreted by the
+    cafe-runner at run time
+    """
+
+    def decorator(func):
         setattr(func, TAGS_DECORATOR_TAG_LIST_NAME, [])
         setattr(func, TAGS_DECORATOR_ATTR_DICT_NAME, {})
         func.__test_tags__.extend(tags)
         func.__test_attrs__.update(attrs)
         return func
-    return wrap
+    return decorator
+
+
+def data_driven_test(dataset_source=None):
+    """Used to define the data source for a data driven test in a
+    DataDrivenFixture decorated Unittest TestCase class"""
+
+    def decorator(func):
+        setattr(func, DATA_DRIVEN_TEST_ATTR, dataset_source)
+        return func
+    return decorator
+
+
+def DataDrivenFixture(cls):
+    """Generates new unittest test methods from methods defined in the
+    decorated class"""
+
+    if not issubclass(cls, TestCase):
+        raise DataDrivenFixtureError
+
+    test_case_attrs = dir(cls)
+    for attr_name in test_case_attrs:
+        if attr_name.startswith(DATA_DRIVEN_TEST_PREFIX) is False:
+            # Not a data driven test, skip it
+            continue
+
+        original_test = getattr(cls, attr_name, None).__func__
+        test_data = getattr(original_test, DATA_DRIVEN_TEST_ATTR, None)
+
+        if test_data is None:
+            # no data was provided to the datasource decorator or this is not a
+            # data driven test, skip it.
+            continue
+
+        for dataset in test_data:
+            # Name the new test based on original and dataset names
+            base_test_name = str(original_test.__name__)[
+                int(len(DATA_DRIVEN_TEST_PREFIX)):]
+            new_test_name = "test_{0}_{1}".format(
+                base_test_name, dataset.name)
+
+            # Create a new test from the old test
+            new_test = FunctionType(
+                original_test.func_code, original_test.func_globals,
+                name=new_test_name)
+
+            # Copy over any other attributes the original test had (mainly to
+            # support test tag decorator)
+            for attr in list(set(dir(original_test)) - set(dir(new_test))):
+                setattr(new_test, attr, getattr(original_test, attr))
+
+            # Change the new test's default keyword values to the appropriate
+            # new data as defined by the datasource decorator
+            new_default_values = []
+            for arg in list(original_test.func_code.co_varnames)[1:]:
+                new_default_values.append(dataset.data[arg])
+            setattr(new_test, "func_defaults", tuple(new_default_values))
+
+            # Add the new test to the TestCase
+            setattr(cls, new_test_name, new_test)
+    return cls
