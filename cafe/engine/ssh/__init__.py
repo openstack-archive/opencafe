58157baa423a82e0f3cba990cbc7ed94c3a3328b
vishal <vishal.kankatala@rackspace.com>
2013-11-11 12:53:49 -0600

    SSH Behaviors
    Change-Id: Iccc571b95b8c312e2019d9ac20455f60d758ef3d

--- cafe/engine/ssh/__init__.py
+++ cafe/engine/ssh/__init__.py
@@ -13,9 +13,3 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 """
-
-from warnings import warn, simplefilter
-simplefilter("default", DeprecationWarning)
-warn("cafe.engine.models.ssh_response has been moved to "
-    "cafe.engine.ssh.models.ssh_response", DeprecationWarning)
-from cafe.engine.ssh.models.ssh_response import SSHKeyResponse, ExecResponse
