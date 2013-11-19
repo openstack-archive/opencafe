c00af9cd2ccae524794f57da12ddf85757ac7988
step6829 <stephen.lowrie@rackspace.com>
2013-11-19 09:37:24 -0600

    Changed logic of executes & fixed name of parameters being called in connect
    * Removed using keyword names in call to _driver.connect() in favor of passing by the order
    *     specified in PEP-249 standards. This still follows standards and avoids overwriting
    *     the connect function inside of clients where the driver did not implement the naming
    *     structure of the PEP-249 standard correctly.
    *
    * Changed logic in execute & execute_many to return proper cursor objects
    
    Change-Id: If61c00ad34b8bea9012e20bb0ee50f733155735b

--- cafe/engine/clients/sql.py
+++ cafe/engine/clients/sql.py
@@ -59,8 +59,7 @@ class BaseSQLClient(BaseClient):
 
         try:
             self._connection = self._driver.connect(
-                data_source_name=data_source_name, user=user,
-                password=password, host=host, database=database)
+                data_source_name, user, password, host, database)
         except AttributeError as detail:
             message = "No connect method found in self._driver module."
             self._log.exception(detail)
@@ -91,7 +90,9 @@ class BaseSQLClient(BaseClient):
         if cursor is None:
             cursor = self._connection.cursor()
 
-        return cursor.execute(operation, parameters)
+        cursor.execute(operation, parameters)
+
+        return cursor
 
     def execute_many(self, operation, seq_of_parameters=None, cursor=None):
         """
@@ -117,7 +118,9 @@ class BaseSQLClient(BaseClient):
         if cursor is None:
             cursor = self._connection.cursor()
 
-        return cursor.executemany(operation, seq_of_parameters)
+        cursor.executemany(operation, seq_of_parameters)
+
+        return cursor
 
     def close(self):
         """
