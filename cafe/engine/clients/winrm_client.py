3efba8f7358d8aed034660227acf8630e76bcf41
Daryl Walleck <daryl.walleck@rackspace.com>
2014-04-23 23:46:53 -0500

    Added error handling for WinRM connections
    * Added handling for WinRMTransportErrors on initial connections
    
    Change-Id: I47f7fafe6d8e8071041cdde7f0578b3acbdfa67d

--- cafe/engine/clients/winrm_client.py
+++ cafe/engine/clients/winrm_client.py
@@ -19,6 +19,7 @@ import time
 # it be installed unless explicitly needed
 try:
     from winrm.protocol import Protocol
+    from winrm.exceptions import WinRMTransportError
 except:
     raise Exception("Dependency 'winrm' missing.")
 
@@ -158,7 +159,10 @@ class WinRMClient(BaseWinRMClient):
                             '{retries} to {host}.'.format(
                                 iteration=iteration, retries=retries,
                                 host=self.host))
-            self.connect(username=self.username, password=self.password)
+            try:
+                self.connect(username=self.username, password=self.password)
+            except WinRMTransportError as exception:
+                self._log.error(exception.message)
             if self.is_connected():
                 return True
             time.sleep(cooldown)
