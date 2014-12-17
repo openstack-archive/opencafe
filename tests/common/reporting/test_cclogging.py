1dc6170b5965252149e16360b79fb8f450ec0e4b
Jose Idar <jose.idar@rackspace.com>
2014-12-17 12:47:17 -0600

    Added improved error messages and fixed dependency issues
     * cclogger now warns when expected enviornment variables
       are unset, instead of throwing exceptions
     * cclogging's init_root_file_handler() now takes a
       log handler as an optional argument (for forward
       compatability with scripts and log parsing mechanisms)
     * If CAFE_MASTER_LOG_FILE_NAME is unset, the root logger
       will default to a NullHandler.
     * Moves unittest-specific logging to the unittest runner
       module.
     * Adds unittests for getLogger() function
    
    Change-Id: I61578109fe7b86fc1bfec8755c710e7f6de9f1f4

---
+++ tests/common/reporting/test_cclogging.py
@@ -0,0 +1,135 @@
+import unittest
+import mock
+from uuid import uuid4
+from requests import Response
+import logging
+from cafe.common.reporting.cclogging import getLogger as CC_getLogger
+from cafe.common.reporting.cclogging import init_root_log_handler as IRLH
+
+
+def mock_os_getenv_verbose(*args, **kwargs):
+    return 'VERBOSE'
+
+def mock_os_getenv_standard(*args, **kwargs):
+    return 'STANDARD'
+
+def mock_os_getenv_unconfigured(*args, **kwargs):
+    return None
+
+@mock.patch('os.getenv', side_effect=mock_os_getenv_verbose)
+def get_verbose_logger(log_name, *args, **kwargs):
+    return CC_getLogger(log_name)
+
+@mock.patch('os.getenv', side_effect=mock_os_getenv_standard)
+def get_standard_logger(log_name, *args, **kwargs):
+    return CC_getLogger(log_name)
+
+@mock.patch('os.getenv', side_effect=mock_os_getenv_unconfigured)
+def get_unconfigured_logger(log_name, *args, **kwargs):
+    return CC_getLogger(log_name)
+
+
+class BaseLoggerTestMixin(object):
+
+    def call_getLogger(self, logger_func, calls=1):
+        logger_name = str(uuid4())
+        logger = None
+        for n in range(0,calls):
+            logger = logger_func(logger_name)
+        return logger
+
+    def assertHandlerCount(self, logger, expected_handler_count, msg=None):
+        handler_count = len(logger.handlers)
+        std_msg = "Logger had {0} handlers but should have had {1}".format(
+                handler_count, expected_handler_count)
+
+        if handler_count != expected_handler_count:
+            self.fail(self._formatMessage(msg, std_msg))
+
+
+class getLogger_Tests(object):
+
+    def test_new_logger_verbose_env_1_handler(self):
+        global get_verbose_logger
+        logger = self.call_getLogger(get_verbose_logger, 1)
+        self.assertHandlerCount(logger, 1)
+
+    def test_existing_logger_verbose_env_1_handler_after_2_calls(self):
+        global get_verbose_logger
+        logger = self.call_getLogger(get_verbose_logger, 2)
+        self.assertHandlerCount(logger, 1)
+
+
+class Normal_getLogger_Tests(
+    unittest.TestCase, getLogger_Tests, BaseLoggerTestMixin):
+
+    def test_new_logger_unconfigured_env_0_handlers(self):
+        global get_unconfigured_logger
+        logger = self.call_getLogger(get_unconfigured_logger, 1)
+        self.assertHandlerCount(logger, 0)
+
+    def test_new_logger_standard_env_0_handlers(self):
+        global get_standard_logger
+        logger = self.call_getLogger(get_standard_logger, 1)
+        self.assertHandlerCount(logger, 0)
+
+    def test_existing_logger_standard_env_0_handlers_after_2_calls(self):
+        global get_standard_logger
+        logger = self.call_getLogger(get_standard_logger, 2)
+        self.assertHandlerCount(logger, 0)
+
+    def test_existing_logger_unconfigured_env_0_handlers_after_2_calls(self):
+        global get_unconfigured_logger
+        logger = self.call_getLogger(get_unconfigured_logger, 2)
+        self.assertHandlerCount(logger, 0)
+
+
+class Root_getLogger_Tests(
+    unittest.TestCase, getLogger_Tests, BaseLoggerTestMixin):
+
+    def call_getLogger(self, logger_func, calls=1):
+        #Always returns root log
+        logger = None
+        for n in range(0,calls):
+            logger = logger_func(None, )
+        return logger
+
+    def init_root_handler(self):
+        logging.getLogger().addHandler(logging.NullHandler())
+
+    def clean_root_log(self):
+        logging.getLogger().handlers = []
+
+    def setUp(self):
+        self.clean_root_log()
+
+    def test_new_logger_unconfigured_env_0_handlers(self):
+        global get_unconfigured_logger
+        logger = self.call_getLogger(get_unconfigured_logger, 1)
+        self.assertHandlerCount(logger, 0)
+
+    def test_new_logger_standard_env_0_handlers(self):
+        global get_standard_logger
+        logger = self.call_getLogger(get_standard_logger, 1)
+        self.assertHandlerCount(logger, 0)
+
+    def test_initialized_root_logger_unconfigured_env_1_handlers_after_2_calls(
+            self):
+        global get_unconfigured_logger
+        self.init_root_handler()
+        logger = self.call_getLogger(get_unconfigured_logger, 2)
+        self.assertHandlerCount(logger, 1)
+
+    def test_initialized_root_logger_standard_env_1_handlers_after_2_calls(
+            self):
+        global get_standard_logger
+        self.init_root_handler()
+        logger = self.call_getLogger(get_standard_logger, 2)
+        self.assertHandlerCount(logger, 1)
+
+    def test_initialized_root_logger_verbose_env_1_handlers_after_2_calls(
+            self):
+        global get_verbose_logger
+        self.init_root_handler()
+        logger = self.call_getLogger(get_verbose_logger, 2)
+        self.assertHandlerCount(logger, 1)
