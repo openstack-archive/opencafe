5b6d76933c9368c3a62e29163976df702a37028a
step6829 <stephen.lowrie@rackspace.com>
2015-11-04 13:02:14 -0600

    Add SubunitReport
    Adds 'subunit' option to --result commandline argument.
    
    SubunitReport outputs to a file subunit_results in the specified directory
    
    Change-Id: I33f418c5d150551c0bde28c5e31aaa30b2324110

---
+++ cafe/plugins/subunit/setup.py
@@ -0,0 +1,26 @@
+# Copyright 2015 Rackspace
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+from setuptools import setup, find_packages
+
+setup(
+    name='cafe_subunit_plugin',
+    version='0.0.1',
+    description='Subunit Plugin for CAFE',
+    author='Rackspace Cloud QE',
+    author_email='cloud-cafe@lists.rackspace.com',
+    url='http://rackspace.com',
+    packages=find_packages(),
+    namespace_packages=['cafe'],
+    install_requires=['python-subunit', 'pytz'],
+    zip_safe=False)
